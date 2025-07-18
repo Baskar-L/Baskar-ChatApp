6a595166639d4f5bbb6ca9e9de1127f6270e5092
Baskar L <139743997+Baskar-L@users.noreply.github.com>
2025-07-19 00:54:10 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
     };
 
     axios
-      .post("/api/user/login", userInfo)
+      .post("https://baskar-chatapp-backend.onrender.com/api/user/login", userInfo)
       .then((response) => {
         if (response.data) {
           toast.success("Login successful");
