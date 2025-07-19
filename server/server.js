ec3dce303f5ba105eaa367c842832f8c3c8af44f
Baskar L <139743997+Baskar-L@users.noreply.github.com>
2025-07-19 10:26:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://baskar-chatapp.vercel.app"],
+    origin: ["https://baskar-chatapp.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
