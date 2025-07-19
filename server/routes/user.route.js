0b28726a6a96878fc89002692731b95c0ba9b9fe
Baskar L <139743997+Baskar-L@users.noreply.github.com>
2025-07-19 10:47:16 +0530

    Update user.route.js

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,3 +1,22 @@
+// import express from "express";
+// import {
+//   allUsers,
+//   login,
+//   logout,
+//   signup,
+// } from "../controller/user.controller.js";
+// import secureRoute from "../middleware/secureRoute.js";
+// const router = express.Router();
+
+// router.post("/signup", signup);
+// router.post("/login", login);
+// router.post("/logout", logout);
+// router.get("/allusers", secureRoute, allUsers);
+
+// export default router;
+
+
+
 import express from "express";
 import {
   allUsers,
@@ -6,11 +25,15 @@ import {
   signup,
 } from "../controller/user.controller.js";
 import secureRoute from "../middleware/secureRoute.js";
+
 const router = express.Router();
 
+// 📝 Auth Routes
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+// 🔐 Protected Route
 router.get("/allusers", secureRoute, allUsers);
 
 export default router;
